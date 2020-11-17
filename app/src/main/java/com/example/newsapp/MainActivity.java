bc14c0e092826236fc42fb0433bc330010d3493c
girishsrivatsa <girishsrivatsa@cse.iitb.ac.in>
2020-11-17 20:57:38 +0530

    added async task

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,14 +1,97 @@
 package com.example.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+import com.google.gson.*;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Whole w=new Whole();
+        try{
+            String j=new Z().execute().get();
+            Log.i("in",j);
+        }
+        catch(InterruptedException e){
+            Log.e("error",String.valueOf(e.getStackTrace()));
+        }
+        catch(ExecutionException e){
+            Log.e("error",String.valueOf(e.getStackTrace()));
+        }
     }
+}
+class Z extends AsyncTask<Whole, Void, String>{
+    @Override
+    protected String doInBackground(Whole... params) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://bing-news-search1.p.rapidapi.com/news?safeSearch=Off&textFormat=Raw")
+                .get()
+                .addHeader("x-bingapis-sdk", "true")
+                .addHeader("x-rapidapi-key", "5be6f2b8e3msh38c3cc89dc461b5p1c7110jsnd243442900ea")
+                .addHeader("x-rapidapi-host", "bing-news-search1.p.rapidapi.com")
+                .build();
+        try{
+            Response response = client.newCall(request).execute();
+            Log.i("data",response.body().string());
+            return response.body().toString();
+        }
+        catch(IOException e){
+            Log.e("error", String.valueOf(e.getStackTrace()));
+            return String.valueOf(e.getStackTrace());
+        }
+    }
+    protected void onPostExecute(String result) {
+        Log.i("info",result);
+    }
+};
+class Whole{
+    String _type;
+    String webSearchUrl;
+    News[] value;
+}
+class News{
+    String _type;
+    String name;
+    String url;
+    Img image;
+    String description;
+    Org[] provider;
+    String datePublished;
+}
+class Img{
+    String _type;
+    Img2 thumbnail;
+    Boolean isLicensed;
+}
+class Img2{
+    String _type;
+    String contentUrl;
+    Integer width;
+    Integer height;
+}
+class Org{
+    String _type;
+    String name;
+    Img image;
 }
